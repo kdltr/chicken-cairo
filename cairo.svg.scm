e715892777e4a8fc13f7cd418741cae2aeb60908
Kooda <kooda@upyum.com>
2017-12-09 20:13:47 +0100

    Export svg-surface-create

--- cairo.svg.scm
+++ cairo.svg.scm
@@ -6,6 +6,7 @@
  (foreign-declare "#include \"cairo.h\"")
  (foreign-declare "#include \"cairo-svg.h\"")
 
+(export svg-surface-create)
 (define svg-surface-create
   (foreign-lambda surface "cairo_svg_surface_create"
                   c-string double double))
