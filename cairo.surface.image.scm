4988751ad28f28ceebb7a1c27e5c1d228a773d47
Vasilij Schneidermann <mail@vasilij.de>
2018-04-26 12:04:39 +0200

    Conditionally define PNG functions

--- cairo.surface.image.scm
+++ cairo.surface.image.scm
@@ -5,6 +5,11 @@
   (include "types.scm")
   (foreign-declare "#include \"cairo.h\"")
 
+  ;; cairo does unfortunately not do this for us
+  (foreign-declare "#ifndef CAIRO_HAS_PNG_FUNCTIONS")
+  (foreign-declare "#define CAIRO_HAS_PNG_FUNCTIONS 0")
+  (foreign-declare "#endif")
+
 (defs
   (surface image-surface-create format int int)
   (surface image-surface-create-for-data c-pointer format int int int)
@@ -13,5 +18,10 @@
   (int image-surface-get-width surface)
   (int image-surface-get-height surface)
   (int image-surface-get-stride surface))
+
+(when (foreign-value "CAIRO_HAS_PNG_FUNCTIONS" bool)
+  (defs
+    (surface image-surface-create-from-png c-string)
+    (status surface-write-to-png! surface c-string)))
 
 )
